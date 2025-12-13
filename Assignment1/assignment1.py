f34bb5ead1e7cbe0f525adb61522958c6376730f
anshriti <shanshriti1121@gmail.com>
2025-12-13 14:11:23 +0545

    Assignment 1

---
+++ Assignment1/assignment1.py
@@ -0,0 +1,29 @@
+#Variables and Input
+my_age = 21
+print("I am",my_age)
+
+food = input("What is your favorite food? ")
+print("Your favorite food is", food)
+
+#Type conversion
+str_num  = "42"
+num = int(str_num)
+print("The converted number is", num)
+
+float_num = 3.14159
+str_num = str(float_num)
+print("The converted string is", str_num)
+
+#Strings
+str1 = "Hello"
+str2 = "World"
+str3 = str1 + " " + str2
+print(str3)
+
+string = "Python"
+print(string[2])
+
+a = input("Enter a string:")
+first_five = a[0:5]
+print("The first 5 words are:", first_five)
+
